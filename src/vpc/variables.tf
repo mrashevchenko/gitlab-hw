bdb361d3da9b87ea0a060652b956b35e29339af5
Mikhail <100411467+mrashevchenko@users.noreply.github.com>
2023-11-15 01:02:47 +0300

    Create variables.tf

---
+++ src/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "env_name" {
+  type    = string
+  description = "Имя облачной сети"
+}
+
+variable "zone" {
+  type    = string
+  description = "Зона, в которой создать подсеть"
+}
+
+variable "cidr" {
+  type    = string
+  description = "CIDR-блок для подсети"
+}
