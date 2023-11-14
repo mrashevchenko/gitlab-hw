0f8f7d195f4227b95b3ab4ad533c354a58d0500c
Mikhail <100411467+mrashevchenko@users.noreply.github.com>
2023-11-15 01:05:40 +0300

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,68 +1,51 @@
-resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
-}
-resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
-}
-
-data "yandex_compute_image" "ubuntu" {
-  family = var.vm_web_family
-}
-resource "yandex_compute_instance" "platform1" {
-  name =  local.vm_web_instance_name
-  platform_id = var.vm_web_platform_id
-  metadata = var.common_metadata
-  resources {
-    cores         = var.vms_resources.vm_web_resources.cores
-    memory        = var.vms_resources.vm_web_resources.memory
-    core_fraction = var.vms_resources.vm_web_resources.core_fraction
-  }
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.image_id
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
     }
   }
-  scheduling_policy {
-    preemptible = true
-  }
-  network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
-  }
+  required_version = ">=0.13"
 }
-data "yandex_compute_image" "ubuntu2" {
-  family = var.vm_db_family
+
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.default_zone
 }
-resource "yandex_compute_instance" "platform2" {
-  name        = local.vm_db_instance_name
-  platform_id = var.vm_db_platform_id
-  metadata    = var.common_metadata
-  resources {
-    cores         = var.vms_resources.vm_db_resources.cores
-    memory        = var.vms_resources.vm_db_resources.memory
-    core_fraction = var.vms_resources.vm_db_resources.core_fraction
-  }
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.image_id
-    }
-  }
-  scheduling_policy {
-    preemptible = true
-  }
-  network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
-  }
+
+module "vpc_dev" {
+  source       = "./vpc"
+  zone = "ru-central1-a"
+  cidr = "10.0.1.0/24"
+  env_name = "develop"
+}
 
 
 
-#  metadata = {
-#    serial-port-enable = 1
-#    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
-#  }
 
+module "test-vm" {
+  source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name        = "develop"
+  network_id      = module.vpc_dev.network_id
+  subnet_zones    = ["ru-central1-a"]
+  subnet_ids      = [module.vpc_dev.subnet_id]
+  instance_name   = "web"
+  instance_count  = 1
+  image_family    = "ubuntu-2004-lts"
+  public_ip       = true
+
+  metadata = {
+      user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+      serial-port-enable = 1
+  }
+
+}
+
+#Пример передачи cloud-config в ВМ для демонстрации №3
+data "template_file" "cloudinit" {
+  template = file("./cloud-init.yml")
+  vars     = {
+    ssh-authorized-keys = file(var.ssh-authorized-keys[0])
+  }
 }
