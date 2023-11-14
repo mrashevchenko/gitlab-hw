71b453203b75a0e4122ff954ef6df4aa3336e851
Mikhail <100411467+mrashevchenko@users.noreply.github.com>
2023-11-15 01:06:20 +0300

    Update outputs.tf

--- src/outputs.tf
+++ src/outputs.tf
@@ -5,3 +5,42 @@ output "dev_network_id" {
 output "dev_subnet_id" {
   value = module.vpc_dev.subnet_id
 }
+root@netology:/opt/terraform/ter-homeworks/04/demo1# cat variables.tf 
+###cloud vars
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network&subnet name"
+}
+
+variable "ssh-authorized-keys" {
+  description = "Path to public SSH key file"
+  type        = list(string)
+  default     = ["~/.ssh/id_ed25519.pub"]
+}
