c1f24edeb03a800ac828d3f34d245585b3f68e8d
Mikhail <100411467+mrashevchenko@users.noreply.github.com>
2023-11-15 01:06:00 +0300

    Update outputs.tf

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,8 +1,7 @@
-output "VMs" {
-  value = {
-    instance_name1 = yandex_compute_instance.platform1.name
-    external_ip1 = yandex_compute_instance.platform1.network_interface.0.nat_ip_address
-    instance_name2 = yandex_compute_instance.platform2.name
-    external_ip2 = yandex_compute_instance.platform2.network_interface.0.nat_ip_address
-  }
+output "dev_network_id" {
+  value = module.vpc_dev.network_id
+}
+
+output "dev_subnet_id" {
+  value = module.vpc_dev.subnet_id
 }
